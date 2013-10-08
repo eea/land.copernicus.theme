e4d9037c616d5118342192c0e8125e687c4f5277
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2013-10-08 17:34:39 +0300

    Refs #16967 Copernicus; Refs #17000 Copernicus; Removed in-situ frozenEntry; Added copernicus.footer.info viewlet

---
+++ land/copernicus/theme/browser/viewlets.py
@@ -0,0 +1,25 @@
+from plone.app.layout.viewlets.common import ViewletBase
+from plone.memoize import ram
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from time import time
+
+
+class CopernicusFooterInfoViewlet(ViewletBase):
+    render = ViewPageTemplateFile('footer_info.pt')
+
+    # we cache the results for one hour
+    @ram.cache(lambda *args: time() // (60*60))
+    def last_update(self):
+        catalog = self.context.portal_catalog
+        results = catalog.searchResults(
+            {'portal_type': ['LandItem', 'LandSection'],
+             'review_state': 'published',
+             'sort_on': 'modified',
+             'sort_order': 'descending'})
+
+        last_update = None
+
+        if results:
+            last_update = results[0].ModificationDate
+
+        return last_update
