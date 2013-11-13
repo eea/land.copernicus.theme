d8682237a565a55aefc9eb85fd903531433d816e
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-11-13 16:51:38 +0200

    Refs #17452 Copernicus; Added design.js browser resource, updated theme stylesheet

---
+++ land/copernicus/theme/browser/design.js
@@ -0,0 +1,15 @@
+jQuery( document ).ready(function($) {
+    var warning_text_element = $('<span/>', {
+                                            text: '(login required)',
+                                            class: 'login-req-warning',
+                               });
+    if ($("body").hasClass("subsection-high-resolution-image-mosaic") || 
+        $("body").hasClass("userrole-anonymous")) {
+        var target_photoalbum = $(".photoAlbumEntryTitle").filter(":contains('Image 2012')");
+        $(target_photoalbum).parents('.photoAlbumEntry').append($(warning_text_element));
+    }
+
+    // trim description, it has too much space before
+    var $description = $("#content .documentDescription");
+    $description.text($.trim($description.text()));
+});
