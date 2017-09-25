7e17b2ea6acc3a60525e29ba95233ef1c05a2331
David Batranu <david.batranu@eaudeweb.ro>
2017-09-25 12:14:10 +0300

    [refs #88278] Optimse JS resources.

--- land/copernicus/theme/upgrades/evolve300.py
+++ land/copernicus/theme/upgrades/evolve300.py
@@ -22,6 +22,7 @@ def _apply(grouper, result):
     return list(chain(*map(grouper, result)))
 
 
+GROUP_ON_EXTERNAL = partial(_group_on, methodcaller('isExternalResource'))
 GROUP_ON_AUTH = partial(_group_on, methodcaller('getAuthenticated'))
 GROUP_ON_REL = partial(_group_on, methodcaller('getRel'))
 GROUP_ON_COOKABLE = partial(_group_on, methodcaller('getCookable'))
@@ -30,9 +31,12 @@ GROUP_ON_EXPR = partial(_group_on, methodcaller('getExpression'))
 GROUP_ON_COND = partial(_group_on, methodcaller('getConditionalcomment'))
 GROUP_ON_RENDER = partial(_group_on, methodcaller('getRendering'))
 
+GROUP_ON_INLINE = partial(_group_on, methodcaller('getInline'))
+
 
 def optimise_css(tool):
-    auth = GROUP_ON_AUTH(tool.resources)
+    external = GROUP_ON_EXTERNAL(tool.resources)
+    auth = _apply(GROUP_ON_AUTH, external)
     rel = _apply(GROUP_ON_REL, auth)
     cook = _apply(GROUP_ON_COOKABLE, rel)
     media = _apply(GROUP_ON_MEDIA, cook)
@@ -42,8 +46,26 @@ def optimise_css(tool):
 
     for sheet in final:
         sheet.setRendering('link')
-        sheet.setCompression('full')
-        sheet.setCacheable(True)
+        if not sheet.isExternalResource():
+            sheet.setCompression('full')
+            sheet.setCacheable(True)
+
+    tool.resources = final
+    tool.cookResources()
+
+
+def optimise_js(tool):
+    external = GROUP_ON_EXTERNAL(tool.resources)
+    auth = _apply(GROUP_ON_AUTH, external)
+    inline = _apply(GROUP_ON_INLINE, auth)
+    cook = _apply(GROUP_ON_COOKABLE, inline)
+    cond = _apply(GROUP_ON_COND, cook)
+    expr = _apply(GROUP_ON_EXPR, cond)
+    final = tuple(chain(*expr))
+
+    for resource in final:
+        if not resource.isExternalResource():
+            resource.setCacheable(True)
 
     tool.resources = final
     tool.cookResources()
@@ -51,3 +73,4 @@ def optimise_css(tool):
 
 def run(_):
     optimise_css(api.portal.get_tool('portal_css'))
+    optimise_js(api.portal.get_tool('portal_javascripts'))
