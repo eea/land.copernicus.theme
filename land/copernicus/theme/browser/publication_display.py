60062c69d8ac10dd7ff6c5f388c8ff0a2687ef23
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-03-16 15:50:29 +0200

    [refs #94018] Fix image title and alt attributes for publication display (the non-ASCII error).

--- land/copernicus/theme/browser/publication_display.py
+++ land/copernicus/theme/browser/publication_display.py
@@ -4,6 +4,10 @@ from Products.Five.browser import BrowserView
 class PublicationDisplay(BrowserView):
     def __call__(self):
         try:
-            return self.context.image.image_thumb
+            image = self.context.image.image_thumb
+            safe_title = self.context.title.decode('utf-8', 'replace')
+            image.title = safe_title
+            image.alt = safe_title
+            return image
         except AttributeError:
             return
