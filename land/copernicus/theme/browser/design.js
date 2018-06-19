810cf6af37d93edd3bb42311801bf49d637d0d36
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-06-19 18:38:30 +0300

    [refs #96460] Get rid of custom deprecated labels (login required) for Image 2012 and VHR 2012.

--- land/copernicus/theme/browser/design.js
+++ land/copernicus/theme/browser/design.js
@@ -1,23 +1,4 @@
 jQuery(document).ready(function($) {
-  var warning_text_element = $('<span/>', {
-    text: '(login required)',
-    'class': 'login-req-warning',
-  });
-
-  if ($('body').hasClass('subsection-high-resolution-image-mosaic') &&
-      $('body').hasClass('userrole-anonymous')) {
-    var target_photoalbum = $('.photoAlbumEntryTitle').filter(':contains("Image 2012")');
-    $(target_photoalbum).parents('.photoAlbumEntry').append($(warning_text_element));
-  }
-
-  var warning_text_element2 = warning_text_element.clone();
-
-  if ($('body').hasClass('subsection-high-resolution-image-mosaic-very-high-resolution') &&
-      $('body').hasClass('userrole-anonymous')) {
-    var target_photoalbum = $('.photoAlbumEntryTitle').filter(':contains("VHR 2012")');
-    $(target_photoalbum).parents('.photoAlbumEntry').append($(warning_text_element2));
-  }
-
   // trim description, it has too much space before
   var $description = $('#content .documentDescription');
   $description.text($.trim($description.text()));
