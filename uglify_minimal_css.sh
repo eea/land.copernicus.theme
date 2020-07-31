0721f1c0ca73cb0f362b7da4751eae836f2fb35f
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-07-31 11:18:41 +0300

    [refs #120299] Add script for uglify css.

---
+++ uglify_minimal_css.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# npm install uglifycss -g
+uglifycss ./land/copernicus/theme/skins/copernicus_theme_styles/minimal.css > ./land/copernicus/theme/skins/copernicus_theme_styles/minimal.min.css
