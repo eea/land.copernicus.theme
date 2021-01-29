2c1a902703456e99cd12e74550151a31aa776e59
GhitaB <ghitabizau27@gmail.com>
2021-01-29 11:26:06 +0200

    [refs #126691] Move js to file.

---
+++ land/copernicus/theme/skins/copernicus_theme_styles/glider_dashboard_items_carousel.js
@@ -0,0 +1,18 @@
+var dashboard_items = document.getElementsByClassName("dashboard-item");
+var container_width = document.getElementsByClassName("main")[0].offsetWidth;
+
+if(dashboard_items.length > 2 && (dashboard_items.length * 150 > container_width)) {
+  document.getElementsByClassName("glider-prev")[0].style.display = '';
+  document.getElementsByClassName("glider-next")[0].style.display = '';
+
+  new Glider(document.querySelector('.dashboards-container'), {
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
+}
